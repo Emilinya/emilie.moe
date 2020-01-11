e66c6dc74dd25f18dadc35f9fd913c65f51d774a
Benito1001 <bendid2103@gmail.com>
2020-01-11 12:30:22 +0100

    Finished lcm finder

--- preprocessor/content/www/projects/math/lcm.js
+++ preprocessor/content/www/projects/math/lcm.js
@@ -2,19 +2,23 @@ const form = document.getElementById('form');
 const input = document.getElementById('input');
 const resultP = document.getElementById('resultP');
 
-form.addEventListener("submit", event => {
+form.addEventListener("submit", async event => {
 	event.preventDefault();
 	let digits = []
 	values = input.value.split(",");
 	for (let i = 0; i < values.length; i++) {
-		if (values[i].trim().length > 6) {
-			input.setCustomValidity('One of the numbers is too big, max 6 digits');
-			return;
-		} else if (digits.length > 49) {
+		if (digits.length > 49) {
 			input.setCustomValidity('Too many numbers, max 50');
 			return;
 		}
-		digits.push(parseInt(values[i]));
+		digits.push(values[i]);
 	}
-	// Communicate with lcm.py
+	let response = await fetch(`http://api.bendik.moe/lcm?values=${digits.join(",")}`);
+	let body = await response.text();
+	if (body === "None") {
+		input.setCustomValidity('Request timed out, lcm is too big');
+		resultP.innerText = "";
+	} else {
+		resultP.innerHTML = "Lowest common multiple: <b><u>" + body + "</u></b>";
+	}
 });
