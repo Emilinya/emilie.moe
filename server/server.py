633e034e36144cf737ab992e31769e42914b0319
Benito1001 <bendid2103@gmail.com>
2020-01-11 12:31:15 +0100

    Made server work with CORS

--- server/server.py
+++ server/server.py
@@ -22,10 +22,17 @@ app = Flask(__name__)
 def lcmify():
 	values = request.args.get("values")
 	values = [int(value) for value in values.split(",")]
-	return f'{escape(timeout(lcm, 1, values))}!'
+	return f'{escape(timeout(lcm, 1, values))}'
 
 @app.route('/diff')
 def diffify():
 	values = request.args.get("values")
 	values = [int(value) for value in values.split(",")]
-	return f'{escape(diff_solver(values))}!'
+	return f'{escape(timeout(diff_solver, 1, values))}!'
+
+@app.after_request
+def after_request(response):
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+	return response
