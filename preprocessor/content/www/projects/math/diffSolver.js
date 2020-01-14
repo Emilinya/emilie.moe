c775c659658cab4831be7865a5c2196cbf270304
Benito1001 <bendid2103@gmail.com>
2020-01-14 21:42:17 +0100

    redesigned diffSolver

--- preprocessor/content/www/projects/math/diffSolver.js
+++ preprocessor/content/www/projects/math/diffSolver.js
@@ -1,186 +1,145 @@
 const form = document.getElementById('form');
-const eqInput = document.getElementById('eqInput');
+const funcInput = document.getElementById('funcInput');
 const eqOutput = document.getElementById('eqOutput');
-const initialsDiv = document.getElementById('initialsDiv');
+const initInput = document.getElementById('initInput');
 const nInput = document.getElementById('nInput');
 const eqSolve = document.getElementById('eqSolve');
 const eqResult = document.getElementById('eqResult');
 const eqValues = document.getElementById('eqValues');
 
-eqInput.addEventListener("keyup", updateData);
-initialsDiv.addEventListener("keyup", updateData);
-nInput.addEventListener("keyup", updateData)
+funcInput.addEventListener("keyup", event => updateData(event));
+initInput.addEventListener("keyup", event => updateData(event));
+nInput.addEventListener("keyup", event => updateData(event))
 
-function updateData() {
-	updateEqData();
+let funcData;
+let initialsData;
+let nData;
+
+function updateData(event) {
+	updateFuncData();
 	updateInitialsData();
 	updateNData();
+	if (event.key !== "Enter") {
+		funcInput.setCustomValidity('');
+		initInput.setCustomValidity('');
+	}
 }
 
-let eqData;
-function updateEqData() {
-	solverArgs = parseInput(eqInput.value);
-	if (!arraysAreEqual(solverArgs, eqData)) {
-		let output;
-		if (solverArgs) {
-			eqData = solverArgs.slice();
-			resetForm();
-			output = [];
-			for (var i = 0; i < solverArgs.length; i++) {
-				if (solverArgs[i] !== 0) {
-					if (solverArgs[i] === 1) {
-						solverArgs[i] = ""
-					} else if (solverArgs[i] === -1) {
-						solverArgs[i] = "-"
-					}
-					output.push(`${solverArgs[i]}x<sub>n-${i+1}</sub>`);
-					addToForm(i);
-				}
+function updateFuncData() {
+	let functions = funcInput.value.split(",").map(value => parseFloat(value));
+	if (!arraysAreEqual(funcData, functions)) {
+		funcData = functions;
+
+		// Parse functions to make output pretty:
+		funcValue = "x<sub>n</sub> = ";
+		for (let i = 0; i < functions.length; i++) {
+			if (functions[i] === 0) {
+				continue;
 			}
-			output = `x<sub>n</sub> = `+output.join(" + ");
-			updateOutput({eq: output});
-		} else {
-			eqData = solverArgs
-			updateOutput({eq: false});
+			let f = Math.abs(functions[i]);
+			let sign = Math.sign(functions[i+1]);
+			if (isNaN(f)) {
+				funcValue = false;
+				break;
+			} else if (f === 1) {
+				f = "";
+			}
+			funcValue += `${f}x<sub>n-${i+1}</sub>`;
+			if (i != functions.length-1) {
+				funcValue += ` ${{"1": "+", "-1": "-"}[sign]} `;
+			}
 		}
+		updateOutput({func: funcValue});
 	}
 }
 
-let initialsData;
 function updateInitialsData() {
-	let formElements = initialsDiv.children;
-	initialsRay = [];
-	valueRay = [];
-	for (let i = 0; i < formElements.length; i++) {
-		if (formElements[i].tagName == "INPUT") {
-			formText = document.getElementById(formElements[i].id.replace("Input", "Text"));
-			index = +formText.innerHTML.replace(/x<sub>(\d*)<\/sub> = /, "$1");
-			initialsRay[index] = +formElements[i].value
-			valueRay.push(formText.innerHTML + formElements[i].value);
+	let initials = initInput.value.split(",").map(value => parseFloat(value));
+	if (!arraysAreEqual(initialsData, initials)) {
+		initialsData = initials;
+
+		// Parse initials to make output pretty:
+		let initValue = "";
+		texts = initials.map((value, i) => {
+			if (isNaN(value)) {
+				initValue = false
+			}
+			return `x<sub>${i}</sub> = ${value}`
+		});
+		if (initValue === "") {
+			initValue = texts.join(", ")
 		}
-	}
-	for (let i = 0; i < initialsRay.length; i++) {
-		if (!initialsRay[i]) {
-			initialsRay[i] = 0
-		}
-	}
-	if (!arraysAreEqual(initialsData, initialsRay)) {
-		initialsData = initialsRay;
-		value = valueRay.join(", ");
-		updateOutput({init: value});
+		updateOutput({init: initValue});
 	}
 }
 
-let nData;
 function updateNData() {
 	if (nData !== nInput.value) {
-		nData = +nInput.value;
-		updateOutput({n: "n = "+nInput.value});
+		if (nInput.value !== "") {
+			nData = +nInput.value;
+			updateOutput({n: "n = "+nInput.value});
+		} else {
+			updateOutput({n: false});
+		}
 	}
 }
 
-let eqOutputTextObj = {eq: "", init: "", n: ""};
+let eqOutputTextObj = {func: "", init: "", n: ""};
 function updateOutput(updateObj) {
 	for (let key in updateObj) {
 		eqOutputTextObj[key] = updateObj[key];
 	}
 	for (let key in eqOutputTextObj) {
 		if (!eqOutputTextObj[key]) {
-			eqOutput.innerHTML = "<span style='color:#ff6666'>Problem with input</span>";
+			eqOutput.innerText = "";
 			return;
 		}
 	}
-	eqOutput.innerHTML = eqOutputTextObj["eq"] + ", " + eqOutputTextObj["init"] + ", " + eqOutputTextObj["n"];
-}
-
-function parseInput(string) {
-	// Get parts of the equation
-	let parts = [];
-	let splitString = string.split(" ");
-	for (let i = 0; i < splitString.length; i++) {
-		if (splitString[i] === "-") {
-			splitString[i+1] = "-"+splitString[i+1];
-		} else if (splitString[i] !== "+" && splitString[i] !== "") {
-			parts.push(splitString[i]);
-		}
+	if (funcData.length !== initialsData.length) {
+		eqOutput.innerText = "";
+		return;
 	}
-
-	// Parse parts
-	solverArgs = [];
-	for (let i = 0; i < parts.length; i++) {
-		splitPart = parts[i].split("x");
-		if (!splitPart[1] || [1] === "") {
-			return false;
-		}
-		if (splitPart[0] === "") {
-			splitPart[0] = "1";
-		} else if (splitPart[0] === "-") {
-				splitPart[0] = "-1";
-		} else if (isNaN(+splitPart[0])) {
-			return false;
-		}
-		solverArgs[splitPart[1]] = +splitPart[0];
-	}
-
-	// Fill empty space with 0
-	for (var i = 0; i < solverArgs.length; i++) {
-		if (!solverArgs[i]) {
-			solverArgs[i] = 0
-		}
-	}
-
-	return solverArgs;
-}
-
-function resetForm() {
-	let child = initialsDiv.lastElementChild;
-	while (child) {
-		initialsDiv.removeChild(child);
-		child = initialsDiv.lastElementChild;
-	}
-}
-
-function addToForm(i) {
-	sub = document.createElement("sub");
-	sub.appendChild(document.createTextNode(i))
-
-	text = document.createElement("span");
-	text.className = "defaultSpan"
-	text.appendChild(document.createTextNode("x"))
-	text.appendChild(sub)
-	text.appendChild(document.createTextNode(" = "))
-	text.id = "formText"+i
-	initialsDiv.appendChild(text)
-
-	input = document.createElement("input");
-	input.type = "number"
-	input.required = true
-	input.style.width = "50px"
-	input.style.marginRight = "20px"
-	input.id = "formInput"+i
-	initialsDiv.appendChild(input)
+	eqOutput.innerHTML = `
+		<span style="color: #ee5555">${eqOutputTextObj["func"]}</span>,
+		<span style="color: #55ee55">${eqOutputTextObj["init"]}</span>,
+		<span style="color: #5555ee">${eqOutputTextObj["n"]}</span>
+	`;
 }
 
 form.addEventListener("submit", async event => {
 	event.preventDefault();
-	if (eqData && eqData.length === initialsData.length) {
-		let response = await fetch(`http://api.bendik.moe/diff?initials=${initialsData.join(",")}&functions=${eqData.join(",")}&N=${nData}&rightSide=0&display=full`);
+	updateData({key: "Enter"})
+	if (funcData.length === initialsData.length) {
+		let response = await fetch(
+			`http://api.bendik.moe/diff?initials=${initialsData.join(",")}
+			&functions=${funcData.join(",")}&N=${nData}&rightSide=0&display=full`
+		);
 		let body = await response.text();
 		if (body === "None" || body === "Error") {
 			eqResult.innerText = "";
 		} else {
-			console.log(body);
 			eqResult.innerText = `First ${nData} values in the equation:`;
 			eqValues.innerHTML = "";
 			resultRay = body.replace(/&#39;/g, "").slice(1, -1).split(", ");
 			for (var i = 0; i < resultRay.length; i++) {
-				console.log(resultRay[i]);
-				li = document.createElement("li");
-				li.style.whiteSpace = "pre";
-				li.appendChild(document.createTextNode(resultRay[i]));
-				eqValues.appendChild(li);
+				const tr = document.createElement("tr");
+				const [indexText, valueText] = resultRay[i].split(":").map(v => v.trim());
+				indexTd = document.createElement("td");
+				indexTd.appendChild(document.createTextNode(indexText+":"));
+				indexTd.className = "noBorderTable";
+				indexTd.style.textAlign = "right";
+				tr.appendChild(indexTd);
+
+				valueTd = document.createElement("td");
+				valueTd.appendChild(document.createTextNode(valueText));
+				valueTd.className = "noBorderTable";
+				tr.appendChild(valueTd);
+				eqValues.appendChild(tr);
 			}
 		}
+	} else {
+		funcInput.setCustomValidity('Functions and initial conditions must have same length');
+		initInput.setCustomValidity('Functions and initial conditions must have same length');
 	}
 });
 
