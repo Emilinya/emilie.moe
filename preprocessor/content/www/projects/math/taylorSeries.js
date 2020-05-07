ab24022fb6b184cbc940e5beac3c6ebb845f1360
Benito1001 <bendid2103@gmail.com>
2020-05-07 20:16:32 +0200

    finished taylor series page

--- preprocessor/content/www/projects/math/taylorSeries.js
+++ preprocessor/content/www/projects/math/taylorSeries.js
@@ -2,6 +2,7 @@ const form = document.getElementById('form');
 const funcInput = document.getElementById('funcInput');
 const orderInput = document.getElementById('orderInput');
 const pointInput = document.getElementById('pointInput');
+const radiusInput = document.getElementById('radiusInput');
 const resultP = document.getElementById('resultP');
 const resultImg = document.getElementById('resultImg');
 
@@ -9,19 +10,33 @@ form.addEventListener("submit", async event => {
 	event.preventDefault();
 
 	func = funcInput.value.replace(/ /g, "").replace(/\+/g, "£")
+	// start_time = +new Date();
 	let response = await fetch(
 		`http://api.bendik.moe/taylor?function=${func}
-		&order=${orderInput.value}&point=${pointInput.value}`
+		&order=${orderInput.value}&point=${pointInput.value}
+		&radius=${radiusInput.value}`
 	);
 	let body = await response.text();
+	// console.log((+new Date() - start_time)/1000 + " s");
 	if (body === "FuncError") {
-		funcInput.setCustomValidity('Can\'t calculate taylor series');
+		showError('Can\'t calculate taylor series');
+	} else if (body === "TimeoutError") {
+		showError('Request timed out');
 	} else if (body !== "Error") {
 		let [latex, image] = body.slice(1, body.length-1).replace(/&#39;/g, "").split(",");
 		resultP.innerHTML = "\\("+latex.replace(/\\\\/g, "\\")+"\\)";
 		MathJax.typesetPromise();
 
 		resultImg.setAttribute("src", "data:image/png;base64,"+image.slice(1, image.length));
-		resultImg.style.height = "700px";
 	}
 })
+
+function showError(errStr) {
+	errSpan = document.createElement("span");
+	errSpan.appendChild(document.createTextNode(errStr))
+	errSpan.style.color = "#ff4343";
+	for (var child of resultP.childNodes) {
+		resultP.removeChild(child)
+	}
+	resultP.appendChild(errSpan)
+}
