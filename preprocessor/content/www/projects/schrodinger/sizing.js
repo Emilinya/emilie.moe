9386f8e51423612d2ea0b6d9058af21cd4bb3d9d
Emilie <emilid2103@gmail.com>
2024-06-10 15:07:54 +0200

    Why is frontend such garbage

--- preprocessor/content/www/projects/schrodinger/sizing.js
+++ preprocessor/content/www/projects/schrodinger/sizing.js
@@ -2,24 +2,35 @@ var topDiv = document.getElementById("topDiv");
 var video = document.getElementById("animVideo");
 video.addEventListener("loadeddata", resize);
 
+var resizeCalled = false;
+
 // I would hope there is a simpler, pure-css way of doing this, but who knows
 function resize() {
-    var ratio = video.offsetWidth/video.offsetHeight;
-    
+    resizeCalled = true;
+    console.debug("Resizing");
+    var ratio = video.offsetWidth / video.offsetHeight;
+
     var availableHeight = innerHeight - topDiv.offsetHeight;
     var availableWidth = topDiv.offsetWidth;
-    var availableRatio = availableWidth/availableHeight;
+    var availableRatio = availableWidth / availableHeight;
 
     if (ratio > availableRatio) {
         // image is wider than available space, limit by width
-        video.style.width = availableWidth+"px";
+        video.style.width = availableWidth + "px";
         video.style.height = "auto";
     } else {
         // image is taller than available space, limit by height
         video.style.width = "auto";
-        video.style.height = availableHeight+"px";
+        video.style.height = availableHeight + "px";
     }
 
     video.style.display = "";
 }
 window.addEventListener("resize", resize);
+
+// sometimes 'loadeddata' does not get called? So here is a hack :)
+setTimeout(() => {
+    if (!resizeCalled) {
+        resize();
+    }
+}, 100);
